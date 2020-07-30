851845131665d5c01323a301e809f4c7001333b2
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-30 09:52:20 -0400

    Second test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shayanne's Portfolio (TEST)
         </a>
       </header>
     </div>
