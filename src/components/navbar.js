d4296fd3bdd0fde26726da605c12752585822096
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-30 22:57:51 -0400

    changed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,25 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
 
 const NavBar = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggle = () => setDropdownOpen(!dropdownOpen);
 
   return (
-    <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">Shayanne's Pagecd</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/portfolio">Portfolio</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/about">About</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/resume">Resume</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+    <div className="mx-auto">
+     
+      <Nav>
+        <NavItem >
+          <NavLink href="/">about me</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/portfolio">portfolio</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/resume">resume</NavLink>
+        </NavItem>
+      </Nav>
     </div>
   );
 }
