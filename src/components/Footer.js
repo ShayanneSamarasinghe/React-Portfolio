1a36837d9496aa77f02c3931ade85c8b268a5c95
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-07-31 01:24:19 -0400

    tweak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className = "container d-flex justify-content-center" id ="socials">
       
         <a href="https://github.com/ShayanneSamarasinghe"  target="_blank"><img className="btn" id="logo" src={logogithub}></img></a>
-        <a href="https://twitter.com/ShayanneSamsara" target="_blank"><img className="btn" id="logo" src={logoig}></img></a>
+        <a href="https://twitter.com/ShayanneSamsara" target="_blank"><img className="btn" id="logo" src=""></img></a>
         <a href="https://www.linkedin.com/in/shayanne-samarasinghe-48569111a/" target="_blank"><img className="btn" id="logo" src=""></img></a>
         <a href="https://www.instagram.com/sammysomething/?hl=en" target="_blank"><img className="btn" id="logo" src=""></img></a>
 
