2e4f5165fa5a3fa5b4c797cc7a979abbe0239ce8
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-08-03 13:25:03 -0400

    Added card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
+
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
 } from 'reactstrap';
 
 const Cards = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+          <CardTitle>{props.name}</CardTitle>
+        </CardBody>
+        <CardBody>
+          <CardLink href={props.repoLink}>Repo Link</CardLink>
+          <CardLink href={props.appLink}>App Link</CardLink>
         </CardBody>
       </Card>
     </div>
+
+
   );
 };
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Cards from "../components/Cards"
 
-const Portfolio = (props) => {
+const Portfolio = () => {
     return (
         <div>
-           
-    
+         <Cards name="NutShell"  repoLink= "https://github.com/wesycool/nutShell" appLink= "https://wesycool.github.io/Project1-Edmund/Project1-Shayanne/"  />
+         <Cards name="PageExchange" repoLink="https://github.com/ChristineClemens/TheSemicolons" appLink="https://obscure-stream-03506.herokuapp.com/"/>
+         <Cards name="Note Taker" repoLink="https://github.com/ShayanneSamarasinghe/Note-Taker" appLink="https://still-badlands-63453.herokuapp.com/"/>     
         </div>
     )
 }
